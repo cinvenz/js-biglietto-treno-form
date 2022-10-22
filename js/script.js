b57584bb12c1a8e572e62caf4ac00898c25ea70c
cinvenz <vincenzobortune@gmail.com>
2022-10-22 19:46:17 +0200

    aggiunto bottone premuto genera secondo display

--- js/script.js
+++ js/script.js
@@ -31,17 +31,30 @@ function calc(){
     let kilometres = document.querySelector('.chilometri').value;
     let age = document.querySelector('.anni').value;
     let priceTotal = document.querySelector('.result').value;
+    let section2 = document.querySelector('.ticket');
+    let generator = document.querySelector('.genera');
 
+    generator.addEventListener('click', function () {
+        section2.classList.add('open');
+    });
+
+    section2.classList.add('open');
 
     if(age === 'Minorenne'){
         document.querySelector('.result').value = Math.round((0.21 * kilometres) * 0.20 * 100) / 100;
+        console.log('Il prezzo totale è:' , document.querySelector('.result').value + '€');
     }
 
     else if(age ==='Over65'){
         document.querySelector('.result').value = Math.round((0.21 * kilometres) * 0.40 * 100) / 100;
+        console.log('Il prezzo totale è:' , document.querySelector('.result').value + '€');
     }
 
     else {
         document.querySelector('.result').value = Math.round((0.21 * kilometres) * 100) / 100;
+        console.log('Il prezzo totale è:' , document.querySelector('.result').value + '€');
     }
+
+
+
 }
