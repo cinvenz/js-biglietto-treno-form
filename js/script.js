91d17310bc354ced9274de4349027a88d5b10573
cinvenz <vincenzobortune@gmail.com>
2022-10-23 12:58:58 +0200

    aggiunto lo stile

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-// Scrivere un programma che chieda all’utente:
-// - Il numero di chilometri da percorrere
-// - Età del passeggero
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// - va applicato uno sconto del 20% per i minorenni
-// - va applicato uno sconto del 40% per gli over 65.
-// MILESTONE 1:
-// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input (per i chilometri e per l'età) e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console o su un semplice div in pagina.
-// MILESTONE 2:
-// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
-
-
-// let kilometres = parseInt(prompt('I chilometri che vuoi percorrere sono?'));
-// let age = parseInt(prompt('quanti anni hai?'));
-// if(age <= 17){
-//     let priceTotal = Math.round((0.21 * kilometres) * 0.20 * 100) / 100;
-//     console.log('Il prezzo totale è:' , priceTotal + '€');
-// } else if(age >= 65) {
-//     let priceTotal = Math.round((0.21 * kilometres) * 0.40 * 100) / 100;
-//     console.log('Il prezzo totale è:' , priceTotal + '€');
-// }
-// else {
-//     let priceTotal = Math.round(kilometres * 0.21 * 100) / 100;
-//     console.log('Il prezzo totale è:' , priceTotal + '€');
-// }
-
 function calc(){
  
     let kilometres = document.querySelector('.chilometri').value;
@@ -38,10 +10,12 @@ function calc(){
 
     generator.addEventListener('click', function () {
         eleOpen.classList.add('open');
+        generator.classList.add('btnborder');
     });
 
    eleClose.addEventListener('click', function () {
         eleOpen.classList.remove('open');
+        generator.classList.remove('btnborder');
     });
 
     
